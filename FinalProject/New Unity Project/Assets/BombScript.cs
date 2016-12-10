4c134f6d452c2bcd439c4ef604392d7503943673
Ethan <elguerito90@gmail.com>
2016-12-10 02:15:21 -0600

    stuff

--- FinalProject/New Unity Project/Assets/BombScript.cs
+++ FinalProject/New Unity Project/Assets/BombScript.cs
@@ -10,22 +10,43 @@ public class BombScript : MonoBehaviour {
     {
         this.bomb_speed = MisslePivotController.rocketSpeed;
         this.transform.parent = null;
-    }
-	
-	// Update is called once per frame
-	void Update () {
+        Rigidbody rb = this.gameObject.GetComponent<Rigidbody>();
+
         float dx = MisslePivotController.currentPlayerPos.x - MisslePivotController.lastPlayerPos.x;
         float dy = MisslePivotController.currentPlayerPos.y - MisslePivotController.lastPlayerPos.y;
         float dz = MisslePivotController.currentPlayerPos.z - MisslePivotController.lastPlayerPos.z;
 
-        float dspeed = Mathf.Sqrt(Mathf.Pow(dx, 2.0f) + Mathf.Pow(dz, 2.0f)) / Time.deltaTime;
-        if(bomb_speed < 0)
+        float dxspeed = dx / Time.deltaTime;
+        float dyspeed = dy / Time.deltaTime;
+        float dzspeed = dz / Time.deltaTime;
+
+        //rb.velocity = new Vector3(bomb_speed, dy, bomb_speed);
+        if(this.bomb_speed < 0.0f)
         {
-            dspeed = -1.0f * dspeed;
+            rb.AddForce(Vector3.back * bomb_speed * 15.0f, ForceMode.Impulse);
         }
+        else
+        {
+            rb.AddForce(Vector3.forward * bomb_speed, ForceMode.Impulse);
+        }
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //float dx = MisslePivotController.currentPlayerPos.x - MisslePivotController.lastPlayerPos.x;
+        //float dy = MisslePivotController.currentPlayerPos.y - MisslePivotController.lastPlayerPos.y;
+        //float dz = MisslePivotController.currentPlayerPos.z - MisslePivotController.lastPlayerPos.z;
+
+        //float dspeed = Mathf.Sqrt(Mathf.Pow(dx, 2.0f) + Mathf.Pow(dz, 2.0f)) / Time.deltaTime;
+        //if(bomb_speed < 0)
+        //{
+        //    dspeed = -1.0f * dspeed;
+        //}
         //Vector3 dir = new Vector3(dx, dy, dz);
         //float dspeed = Mathf.Abs(1.0f - (dir.y / (dir.x + dir.y + dir.z))) * this.bomb_speed;
 
-        transform.Translate(Vector3.forward * (dspeed) * Time.deltaTime);
+        //transform.Translate(Vector3.forward * (dspeed) * Time.deltaTime);
+        //transform.Translate(Vector3.forward * (bomb_speed) * Time.deltaTime);
     }
 }
