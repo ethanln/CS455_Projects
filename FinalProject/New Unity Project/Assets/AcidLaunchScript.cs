ada4f1dfb1200fab7df4c83d77556d7bd4006ec5
Ethan <elguerito90@gmail.com>
2016-12-10 04:10:54 -0600

    stuff

---
+++ FinalProject/New Unity Project/Assets/AcidLaunchScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AcidLaunchScript : MonoBehaviour {
+
+    public GameObject acid_obj;
+    public GameObject player;
+
+    public float radius_monster_boundary;
+
+    public float current_launch_delay;
+    public float acid_launch_delay_limit;
+
+	// Use this for initialization
+	void Start () {
+        this.current_launch_delay = 0.0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if((this.player.transform.position - this.transform.position).magnitude <= this.radius_monster_boundary)
+        {
+            this.current_launch_delay += Time.deltaTime;
+            if (this.current_launch_delay >= acid_launch_delay_limit)
+            {
+                this.current_launch_delay = 0.0f;
+                GameObject obj = Instantiate(this.acid_obj, this.gameObject.transform.position, this.gameObject.transform.rotation) as GameObject;
+                Physics.IgnoreCollision(obj.GetComponent<Collider>(), this.player.GetComponent<Collider>());
+            }
+        }
+        else
+        {
+            this.current_launch_delay = 0;
+        }
+    }
+}
---
+++ FinalProject/New Unity Project/Assets/AcidScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AcidScript : MonoBehaviour
+{
+
+    public float life_span;
+    public float acid_speed;
+    private Vector3 target;
+    private Vector3 prev_pos;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.prev_pos = this.transform.position;
+        foreach (GameObject b in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            this.target = b.transform.position;
+            break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.life_span -= Time.deltaTime;
+
+        float dx = this.transform.position.x - this.prev_pos.x;
+        float dy = this.transform.position.y - this.prev_pos.y;
+        float dz = this.transform.position.z - this.prev_pos.z;
+
+        this.target.x += dx;
+        this.target.y += dy;
+        this.target.z += dz;
+
+        float step = acid_speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(this.transform.position, this.target, step);
+
+
+        if (this.life_span <= 0.0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Player")
+        {
+            MisslePivotController.rocketSpeed = 2.0f;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        string s = "stuff";
+    }
+}
